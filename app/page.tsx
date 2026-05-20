82fdb2c58727ed2cc14f6dc145639849ebc84aaf
kevinpaulose5-oss <kevinpaulose5@gmail.com>
2026-05-20 17:35:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import DashboardShell from "@/components/layout/dashboard-shell";
+import DashboardShell from "../components/layout/dashboard-shell";
 
 export default function HomePage() {
   return <DashboardShell />;
